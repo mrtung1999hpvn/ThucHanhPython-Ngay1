b0a6a7a583a455bc19fdca978721d52b3419e721
ThanhTung <63633930+mrtung1999hpvn@users.noreply.github.com>
2020-11-04 16:23:22 +0700

    Cau 50
    Viết chương trình đọc một ma trận các số nguyên từ file “matran.txt”. Sắp xếp ma trận theo chiều tăng dần của các cột và ghi kết quả
    vào file “ketqua.txt”
    
    n x m
    n hang m cot
    INPUT 
    3 4
    4 3 2 2
    1 4 10 5
    10 4 0 5

---
+++ Cau 50.py
@@ -0,0 +1,15 @@
+import random
+KetQua=''
+f = open('matran.txt', 'r')
+matran = f.read().split('\n')
+n = int(matran[0].split(' ')[0]) #Hang
+m = int(matran[0].split(' ')[1]) #Cot
+print(f'Hang {n}')
+print(f'Cot {m}')
+matran.pop(0)
+_list = []
+for i in range (n):
+  _list.append([matran[i].split(' ')[j] for j in range (m)])
+  print(_list)
+
+#print(_list)
